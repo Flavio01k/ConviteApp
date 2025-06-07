2165c1d820c683cb3934fbc9be3265205b8dcbfc
Flavio Magalhaes <flaviomg9@gmail.com>
2025-06-07 08:37:11 +0100

    add assets

--- backend/gerador.js
+++ backend/gerador.js
@@ -1,10 +1,9 @@
-// backend/gerador.js
 const { PDFDocument, StandardFonts, rgb } = require('pdf-lib');
 
 async function gerarPdfConvite(nome1, nome2) {
   const pdfDoc = await PDFDocument.create();
   const page = pdfDoc.addPage([600, 400]);
-  const font = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+  const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
 
   page.drawText('Convite Especial', { x: 50, y: 350, size: 24, font, color: rgb(0, 0, 0) });
   page.drawText(`${nome1} & ${nome2}`, { x: 50, y: 250, size: 18, font, color: rgb(0, 0, 0) });
@@ -14,3 +13,10 @@ async function gerarPdfConvite(nome1, nome2) {
 }
 
 module.exports = { gerarPdfConvite };
+
+/*
+caso eu queira uma fonte na net
+const response = await fetch('https://example.com/minha-fonte.ttf');
+const fontBytes = await response.arrayBuffer();
+const customFont = await pdfDoc.embedFont(fontBytes);
+ */
